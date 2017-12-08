0d5a037e2dedadba36cd359607c8990c98080bf2
dralois <epple.alexander@web.de>
2017-12-09 00:28:38 +0100

    Smooth camera follow
    -Added testlevel for camera follow
    -Added camera follow script
    -Added testobject movement script

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    //-----------------------------------------------
+    //Kamera Target
+    //-----------------------------------------------
+    [SerializeField]
+    private Transform m_Target;
+    //-----------------------------------------------
+    //Sonstiges
+    //-----------------------------------------------
+    private Rigidbody m_TargetRB;
+    private Camera m_Camera;
+    //-----------------------------------------------
+    //Einstellungen
+    //-----------------------------------------------
+    public float m_SmoothSpeed;
+    public float m_MaxSpeed;
+    public float m_MinFOV;
+    public float m_MaxFOV;
+
+    private void Start()
+    {
+        //-----------------------------------------------
+        //Hole Components
+        //-----------------------------------------------
+        m_Camera = gameObject.GetComponent<Camera>();
+        m_TargetRB = m_Target.GetComponent<Rigidbody>();
+        //-----------------------------------------------
+        //Debug-Vorsorge
+        //-----------------------------------------------
+        if (m_TargetRB == null) Debug.LogError("Target has no Ridigbody");
+    }
+
+    //-----------------------------------------------
+    //Bewegt die Kamera jedes Fixed Update
+    //-----------------------------------------------
+    private void FixedUpdate()
+    {
+        //-----------------------------------------------
+        //Bestimme Wert zw. 0-1 basierend auf Velocity
+        //-----------------------------------------------
+        float l_LerpVal = Mathf.Min(m_TargetRB.velocity.magnitude / m_MaxSpeed, 1);
+        //-----------------------------------------------
+        //Passe Kamera FOV an
+        //-----------------------------------------------
+        m_Camera.fieldOfView= Mathf.Lerp(m_MinFOV, m_MaxFOV, l_LerpVal);
+        //-----------------------------------------------
+        //Interpolation Wert
+        //-----------------------------------------------
+        float interpolation = m_SmoothSpeed * Time.deltaTime;
+        //-----------------------------------------------
+        //Update Position
+        //-----------------------------------------------
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, m_Target.position.x, interpolation),
+                                         transform.position.y,
+                                         Mathf.Lerp(transform.position.z, m_Target.position.z, interpolation));
+    }
+}
