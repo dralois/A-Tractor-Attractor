d8199d6b72d533f0105de2c8c42716b2971cfa75
Matthias <matsigiga@gmail.com>
2017-12-09 18:16:54 +0100

    automove

---
+++ Assets/Scripts/VehicleAutoMove.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleAutoMove : MonoBehaviour {
+
+    public GameObject[] Checkpoints;
+    public int PlayerIndex;
+
+    private int checkpointIndex;
+
+	// Use this for initialization
+	void Awake () {
+        checkpointIndex = 0;
+        for (int i = 0; i < Checkpoints.Length; i++)
+        {
+            var collider = Checkpoints[i].GetComponentInChildren<CheckpointCollider>();
+            collider.SetIndex(i);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Transform GetCurrentCheckpoint()
+    {
+        return Checkpoints[checkpointIndex].transform;
+    }
+
+    public void OnTrigger(Collider other, int idx)
+    {
+        if(other.tag == "Traktor")
+        {
+            var vehicle = other.GetComponent<VehicleScript>();
+            if(vehicle.playerIndex == PlayerIndex && checkpointIndex <= idx && checkpointIndex < Checkpoints.Length-1)
+            {
+                checkpointIndex++;
+            }
+        }
+    }
+
+}
--- Assets/Scripts/VehicleScript.cs
+++ Assets/Scripts/VehicleScript.cs
@@ -10,6 +10,7 @@ public class VehicleScript : MonoBehaviour
     public float sidewaysDamping = 0.5f;
     public float enemyBeamScaling = 1.0f;
     public float maxAngularVelocity = 1.0f;
+    public float autoSpeed = 1.0f;
     public UfoController[] ufos;
     public Transform[] corners;
 
@@ -21,18 +22,20 @@ public class VehicleScript : MonoBehaviour
     private Vector3 resetPoint;
 
     private Vector3 previousVelocity;
-    
+
     //private variables
     private Rigidbody rb;
-    
+
+    public VehicleAutoMove vehicleAutoMove;
+
     void Start()
     {
         rb = this.gameObject.GetComponent<Rigidbody>();
         rb.maxAngularVelocity = maxAngularVelocity;
 
         resetPoint = rb.position;
-    }    
-	
+    }
+
     void clampRotation()
     {
         Vector3 euler = this.rb.rotation.eulerAngles;
@@ -40,11 +43,13 @@ public class VehicleScript : MonoBehaviour
         euler.x = 0;
         rb.rotation = Quaternion.Euler(euler);
     }
-    
+
     void FixedUpdate()
     {
         if (CountDown.CountdownActive)
-            { return; }
+        { return; }
+
+        bool ufoIsPulling = false;
 
         //calculate forces on corners and apply it to the rigidbody
         for (int i = 0; i < 4; i++)
@@ -59,6 +64,8 @@ public class VehicleScript : MonoBehaviour
                 if (!ufo.isPulling())
                     continue;
 
+                ufoIsPulling = true;
+
                 //Debug.Log("inside");
                 applyForce = true;
 
@@ -66,9 +73,7 @@ public class VehicleScript : MonoBehaviour
                 Vector3 cornerToUfo = ufoAdaptedHeight - corners[i].position;
                 Vector3 ufoForce;
 
-                //cornerToUfo.Normalize();
                 ufoForce = cornerToUfo.normalized * beamStrength;
-                //Debug.Log(cornerToUfo);
 
                 force += ufoForce;
             }
@@ -76,6 +81,15 @@ public class VehicleScript : MonoBehaviour
             //apply force on rigidbody
             if (applyForce)
                 rb.AddForceAtPosition(force, corners[i].position);
+        }
+
+        if (ufoIsPulling == false)
+        {
+            var target = vehicleAutoMove.GetCurrentCheckpoint();
+            if (target != null)
+            {
+                rb.MovePosition(rb.position + (new Vector3(target.position.x, rb.position.y, target.position.z) - rb.position).normalized * Time.fixedDeltaTime * autoSpeed);
+            }
         }
 
         //damp sideways velocity
@@ -92,8 +106,15 @@ public class VehicleScript : MonoBehaviour
         }
 
 
-                //Rotiert das objekt in richtung der aktuellen geschwindigkeit 
+        //Rotiert das objekt in richtung der aktuellen geschwindigkeit 
         clampRotation();
+
+        //if (ufoIsPulling == false)
+        //{
+        //    Debug.Log(rb.velocity);
+        //    float rotationSpeed = 2 * Time.fixedDeltaTime * 100;
+        //    rb.rotation = Quaternion.RotateTowards(rb.rotation, Quaternion.LookRotation(new Vector3(0,0, rb.velocity.z)/*rb.velocity*/, Vector3.up), rotationSpeed);
+        //}
         if (rb.velocity.magnitude > 0.5f)
         {
             float rotationSpeed = 2 * Time.deltaTime * rb.velocity.magnitude;
@@ -101,6 +122,7 @@ public class VehicleScript : MonoBehaviour
                 rotationSpeed = 200 * Time.deltaTime;
             rb.rotation = Quaternion.RotateTowards(rb.rotation, Quaternion.LookRotation(rb.velocity, Vector3.up), rotationSpeed);
         }
+
     }
 
     public void setResetPoint(Vector3 pos)
