9624242f3543304f2294e34790f512bf47a74572
zengerle <ga42zax@mytum.de>
2017-12-09 22:05:54 +0100

    added turn counter

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -17,22 +17,66 @@ public class FinishLine : MonoBehaviour {
     [SerializeField]
     private AudioClip WinSound;
 
+
+    private int p1RoundCounter;
+    private int p2RoundCounter;
+    [SerializeField]
+    private Text p1TurnGui;
+    [SerializeField]
+    private Text p2TurnGui;
+
+
     private void Start()
     {
         gameObject.GetComponent<MeshRenderer>().material.mainTextureScale =  new Vector2(transform.localScale.x, transform.localScale.z);
+        p1RoundCounter = 1;
+        p2RoundCounter = 1;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.name == PlayerOne.name)
         {
-            Winner.text = PlayerOneName.ToUpper() + " WINS!";
-            SoundManager.Instance.PlaySingle(WinSound);
+            if (Vector3.Dot(other.GetComponent<Rigidbody>().velocity, this.transform.forward) > 0)
+            {
+                if (p1RoundCounter == 3)
+                {
+                    Winner.text = PlayerOneName.ToUpper() + " WINS!";
+                    SoundManager.Instance.PlaySingle(WinSound);
+                }
+                else
+                {
+                    p1RoundCounter++;
+                }
+            }
+            else
+            {
+                p1RoundCounter--;
+            }
+
+            p1TurnGui.text = p1RoundCounter + "/3";
         }
+
         else if(other.name == PlayerTwo.name)
         {
-            Winner.text = PlayerTwoName.ToUpper() + " WINS!";
-            SoundManager.Instance.PlaySingle(WinSound);
+            if (Vector3.Dot(other.GetComponent<Rigidbody>().velocity, this.transform.forward) > 0)
+            {
+                if (p2RoundCounter == 3)
+                {
+                    Winner.text = PlayerTwoName.ToUpper() + " WINS!";
+                    SoundManager.Instance.PlaySingle(WinSound);
+                }
+                else
+                {
+                    p2RoundCounter++;
+                }
+            }
+            else
+            {
+                p2RoundCounter--;
+            }
+
+            p2TurnGui.text = p2RoundCounter + "/3";
         }
     }
 }
