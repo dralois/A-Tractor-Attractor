d55dda4f1b41ee81034a03cf138f64cbba551c57
marian <marian@becher-home.net>
2017-12-09 09:00:20 +0100

    fixed trackbeam lighting

---
+++ Assets/Scripts/SplitScreen/LightSelector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSelector : MonoBehaviour {
+
+    [SerializeField]
+    List<Light> playerSpecificLights;
+    [SerializeField]
+    LayerMask showLigtsInLayer;
+
+    void OnPreCull()
+    {
+        foreach (Light light in playerSpecificLights)
+        {
+            if (showLigtsInLayer == (showLigtsInLayer | (1 << light.gameObject.layer)))
+            {
+                light.enabled = true;
+            }
+        }
+    }
+
+    void OnPostRender()
+    {
+        foreach (Light light in playerSpecificLights)
+        {
+            light.enabled = false;
+        }
+    }
+}
