10e74cf91f0ef8088ea7c01e47433c1b62a35240
dralois <epple.alexander@web.de>
2017-12-09 09:42:42 +0100

    Added finishline

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class FinishLine : MonoBehaviour {
+
+    [Header("Settings")]
+    [SerializeField]
+    private Transform PlayerOne;
+    [SerializeField]
+    private Transform PlayerTwo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == PlayerOne.name)
+        {
+            Debug.Log("Player One wins!");
+        }
+        else if(other.name == PlayerTwo.name)
+        {
+            Debug.Log("Player Two wins!");
+        }
+    }
+}
